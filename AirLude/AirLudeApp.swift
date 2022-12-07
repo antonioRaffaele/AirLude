af2cbf7cd731edd9097034b3ccb4610a931f0360
valemosca00 <vmosca22@fed.idserve.net>
2022-12-07 11:27:49 +0100

    first commit

---
+++ AirLude/AirLudeApp.swift
@@ -0,0 +1,20 @@
+//
+//  AirLudeApp.swift
+//  AirLude
+//
+//  Created by Valerio Mosca on 07/12/22.
+//
+
+import SwiftUI
+
+@main
+struct AirLudeApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
